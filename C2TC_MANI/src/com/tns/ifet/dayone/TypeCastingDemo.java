795341be7fa05fc2b779fe1718b80ca8bbeaac00
ELCOT <ELCOT@Lenovo>
2024-11-13 10:12:36 +0530

    try

--- C2TC_MANI/src/com/tns/ifet/dayone/TypeCastingDemo.java
+++ C2TC_MANI/src/com/tns/ifet/dayone/TypeCastingDemo.java
@@ -7,7 +7,7 @@ public class TypeCastingDemo {
 		int i = b; 
 		System.out.println(i);   
 		float f = 22.14f;
-		double d = f;  
+		double d = f; 
 		System.out.println(d);   
 		char ch = 'A'; 
 		int i3 = ch; 
